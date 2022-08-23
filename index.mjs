b555e3cbd0a7375280b1796cf9cb3f3015d23809
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-08-23 12:16:46 +0300

    add webpack

--- index.mjs
+++
@@ -1,5 +0,0 @@
-import ancientsData from './data/ancients.mjs';
-
-const cthulthu = document.getElementById('Cthulthu');
-
-cthulthu.innerHTML = ancientsData[0].cardFace;
---
+++ my-app/src/index.js
@@ -0,0 +1,5 @@
+import ancientsData from './data/ancients.mjs';
+
+const cthulthu = document.getElementById('Cthulthu');
+
+cthulthu.style.backgroundImage = `url(${ancientsData[0].cardFace})`;
