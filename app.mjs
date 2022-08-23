17afd81bdba5370cbf246f84fef4cc3d3ee07e86
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-08-23 12:03:22 +0300

    add index.html

--- app.mjs
+++
@@ -1,3 +0,0 @@
-import ancientsData from './data/ancients.mjs'
-
-console.log(ancientsData[0].cardFace);
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import ancientsData from './data/ancients.mjs';
+
+const cthulthu = document.getElementById('Cthulthu');
+
+cthulthu.innerHTML = ancientsData[0].cardFace;
