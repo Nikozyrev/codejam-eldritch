b25e5a2143c668bce4ec189b9c6502215aa00563
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-08-25 17:42:35 +0300

    webpack

--- my-app/src/data/mythicCards/index.mjs
+++
@@ -1,7 +0,0 @@
-import brownCards from './brown/index.mjs';
-import blueCards from './blue/index.mjs';
-import greenCards from './green/index.mjs';
-
-const allCards = [...blueCards, ...brownCards, ...greenCards];
-
-export { allCards }
